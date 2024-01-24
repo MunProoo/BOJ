fde92bf1abc4e5b0a5029e737197555d6280f668
MunProoo <mjo5178@gmail.com>
2024-01-24 16:19:12 +0900

    2516 풀이

--- platinum/2516/main.go
+++ platinum/2516/main.go
@@ -17,6 +17,11 @@ var (
 	cage      Cage
 )
 
+const (
+	Cage1 = iota
+	Cage2
+)
+
 type Cage struct {
 	cage1Map map[int]interface{}
 	cage1    []int
@@ -39,31 +44,34 @@ func main() {
 		번호대로 앙숙의 수(M), 앙숙관계 번호
 
 		풀이 :
-		1번 원숭이는 무조건 cage1에 넣는다
-		1번 원숭이의 앙숙중 첫번째는 cage1에 넣는다.
-			앙숙 2번째, 3번째는 cage2에 넣는다.
-		cage에 넣어진 원숭이는 visited를 true로 한다.
+		반씩 1,2에 넣는다
+		1에서 안되는 애는 2에 넣는다
+		2에서 안되는 애는 1에 넣는다
 
-		* cage1에서 앙숙관계에 대해 확인이 필요하다.
-		** cage2에 적어도 1마리는 넣어야 한다.
+		둘 다 문제 없이 되도록 계속 반복한다
+		-> 무한 루프 돌게 됨.
+		그럼 어카지
 
-
-		로직 문제점
-		enemy가 대상보다 크면 아직 안들어갔을 확률이 있음. ->  뒤늦게 들어올 수 있음
-		enemy의 앙숙이 나밖에 없으면 입구컷 안당하고 cage1 들어온다.
-
-		ex )4 <-> 5
-		    5 <-> 4,6
-			6 <-> 5
-			일 때, 6을 아직 확정짓지 않은 상태에서 5를 체크해서 cage1에 넣은경우,
-			추후 6의 가능성에서 'cage1 넣어도 된다고' 계산됨 (cage1에 앙숙은 5 1마리밖에 없으니까).
-
-			before : 6이 들어온다 -> 앙숙을 체크했을 때 넣어도 되는가? 에서
-			after : 6이 들어온다 -> 대상이 들어갔을 때 위법되는가? -> 본인이 들어갔을 때 앙숙이 위법되는가?
+		일단 다 밀어넣고, 앞번호 에서부터 앙숙이 2마리인 애들은 걸러내면 되려나
+		체크해야 하는 거
+		1. cage1에 본인 앙숙이 2마리 이상 있다 -> cage1에서 빼야한다.
+		2. cage2에 내가 들어가면, 안되는 애들이 있다. (본인 앙숙들의 앙숙카운트 필요) -> 패스해야한다.
 
 
+		*** 최종 풀이
+		그리디한 생각!!
+		1. 한곳에 몰아 넣는다.
+		2. cage1에서 안되는 애들을 cage2로 넣는다.
+		3. cage2에서 안되는 애들을 cage1로 넣는다.
+
+		작업 없을때까지 반복한다. (난 동시에 하려해서 안됨)
 
 	*/
+
+	// readFile, _ := os.Open("D:\\GoWorkspace\\src\\algorithm\\platinum\\2516\\input.txt")
+	// readFile, _ := os.Open("D:\\GoWorkspace\\src\\algorithm\\platinum\\2516\\예제2.txt")
+	// readFile, _ := os.ReadFile("input.txt")
+	// reader = bufio.NewReader(readFile)
 
 	reader = bufio.NewReader(os.Stdin)
 	// scanner := bufio.NewScanner(reader)
@@ -93,8 +101,30 @@ func main() {
 	cage.cage2Map = make(map[int]interface{})
 	cage.cage2 = make([]int, 0)
 
-	for i := 1; i <= N; i++ {
-		putMonkeyInCage(i)
+	// 원숭이들 한우리에 밀어넣음
+	putMonkeyInCage(N)
+
+	cageType := Cage1 // 다음 작업은 어느 cage에서 해야하는가 값
+	var repeatedFlag bool
+
+	for {
+		if cageType == Cage1 {
+			cageType, repeatedFlag = cage1에서내쫓기()
+		} else {
+			cageType, repeatedFlag = cage2에서내쫓기()
+		}
+
+		if !repeatedFlag {
+			break
+		}
+	}
+
+	for monkey := range cage.cage1Map {
+		cage.cage1 = append(cage.cage1, monkey)
+	}
+
+	for monkey := range cage.cage2Map {
+		cage.cage2 = append(cage.cage2, monkey)
 	}
 
 	if len(cage.cage2) == 0 {
@@ -139,58 +169,56 @@ func readLine() string {
 	return input
 }
 
-func putMonkeyInCage(i int) {
-	// i : 대상     enemy : 앙숙
-
-	if !visited[i] {
-		isPossibleCage1 := isPossiblePushCage1(i) // cage1에 넣어도 되는지 앙숙관계 확인
-		if isPossibleCage1 {
-			cage.cage1Map[i] = true
-			cage.cage1 = append(cage.cage1, i) // 실제 케이지에 넣는다
-			visited[i] = true
-		} else {
-			cage.cage2Map[i] = true
-			cage.cage2 = append(cage.cage2, i) // 실제 케이지에 넣는다
-			visited[i] = true
-		}
-	}
-
-	for enemy := range enemyList[i] {
-		if visited[enemy] {
-			continue
-		}
-		putMonkeyInCage(enemy)
+// 일단 전부 cage1로
+func putMonkeyInCage(N int) {
+	for i := 1; i <= N; i++ {
+		cage.cage1Map[i] = true
 	}
 }
 
-func isPossiblePushCage1(monkey int) bool {
-	// 앙숙을 기준 가능성 체크 (대상을 cage1에 넣어도 되는가)
-	cnt := findCage1EnemyCnt(monkey)
-
-	if cnt >= 2 {
-		return false
+// Cage에 있어도 되는지 확인
+func isPossibleInCage(monkey int, flag int) bool {
+	var anyCage map[int]interface{}
+	if flag == 1 { // cage1 체크
+		anyCage = cage.cage1Map
+	} else { // cage2 체크
+		anyCage = cage.cage2Map
 	}
 
-	// 대상 기준으론 넣어도 됨. 앙숙들은 대상이 들어와도 괜찮은지 확인
-	cage.cage1Map[monkey] = true // 임시로 넣어놓는다
-	for enemy := range enemyList[monkey] {
-		enemyCount := findCage1EnemyCnt(enemy)
-		if enemyCount >= 2 && cage.cage1Map[enemy] != nil { // 기존에 넣은 원숭이에게 문제생김
-			cage.cage1Map[monkey] = nil // 넣으면 안되니까 다시 뺀다
-			return false
-		}
-	}
-
-	return true
-}
-
-// Cage1에 앙숙이 몇 마리 들어있는지 체크
-func findCage1EnemyCnt(monkey int) int {
+	// 본인이 들어가도 되는지 여부 확인
 	cnt := 0
 	for enemy := range enemyList[monkey] {
-		if _, isExist := cage.cage1Map[enemy]; isExist {
+		if _, isExist := anyCage[enemy]; isExist {
 			cnt++
 		}
 	}
-	return cnt
+
+	return cnt < 2
+}
+
+func cage1에서내쫓기() (cageType int, repeatedFlag bool) {
+	for monkey := range cage.cage1Map {
+		// 원숭이가 cage1에서 못있는 애인지 확인
+		if possible := isPossibleInCage(monkey, 1); !possible {
+			// cage1에 못있으면 cage2로 이동시키고 확인
+			cage.cage2Map[monkey] = true
+			delete(cage.cage1Map, monkey)
+			repeatedFlag = true
+		}
+	}
+
+	return Cage2, repeatedFlag
+}
+
+func cage2에서내쫓기() (cageType int, repeatedFlag bool) {
+	for monkey := range cage.cage2Map {
+		// 원숭이가 cage2에서 못있는 애인지 확인
+		if possible := isPossibleInCage(monkey, 2); !possible {
+			// cage2에 못있으면 cage1로 이동시키고 확인
+			cage.cage1Map[monkey] = true
+			delete(cage.cage2Map, monkey)
+			repeatedFlag = true
+		}
+	}
+	return Cage1, repeatedFlag
 }
